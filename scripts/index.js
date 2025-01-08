024fcca17d0be6c10e0809d98ba368d45d7aa693
VladimirKarelin <karelinkarelin.kv@yandex.ru>
2025-01-08 19:04:23 +0300

    index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,34 @@
 // @todo: Темплейт карточки
-
+   const cardTemplate = document.querySelector('#card-template').content;
 // @todo: DOM узлы
-
+   const placesItem = document.querySelector('.places__item');
 // @todo: Функция создания карточки
-
+   function createCard (element) {
+      const addCard = cardTemplate.querySelector('.card').cloneNode(true);
+      addCard.querySelector('.card__image').src = element.link;
+      addCard.querySelector('.card__image').alt = element.name;
+      addCard.querySelector('.card__title').textContent = element.name;
+   }
 // @todo: Функция удаления карточки
+   // const closeButton = document.querySelector('.card__delete-button');
+   //    closeButton.addEventListener('click', function (){
+   //       const deleteCard = closeButton.closest('.card');
+   //       deleteCard.remove();
+   //    });
 
 // @todo: Вывести карточки на страницу
+      function cards (item) {
+         item.forEach(element => {
+            const cardElement = createCard(element);
+            placesItem.append(cardElement);
+         });
+
+         
+      }
+      
+      cards(initialCards);
+
+      console.log(createCard());
+
+
+ 
